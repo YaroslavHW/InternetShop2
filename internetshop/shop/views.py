b61a580347762b9bbc9a281e2d1e97a868aacfac
Ярик <ya31012007@gmail.com>
2024-08-31 23:37:16 +0500

    добавил отзывы

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 from .models import Product
+from .models import Review
 
 # Create your views here.
 
@@ -12,6 +14,14 @@ def home(request):
 
 def view_product(request, id):
     product = Product.objects.filter(id=id).first()
+
+    if request.method == "POST":
+        author = request.POST.get('author')
+        rating = request.POST.get('rating')
+        text = request.POST.get('text')
+        review = Review(author=author, rating=int(rating), text=text,product=product)
+        review.save()
+
     reviews = product.review_set.all()
 
     return render(request, template_name='product.html', context={
